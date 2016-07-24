f89e8b8e30cf682c833d7372b938feb482d16b94
Claudia McCormack <mccormack.chc@gmail.com>
2016-07-24 16:24:30 -0400

    added model methods

--- 717/contact-app/app/models/contact.rb
+++ 717/contact-app/app/models/contact.rb
@@ -1,2 +1,15 @@
 class Contact < ActiveRecord::Base
+
+  def friendly_update
+    updated_at.strftime("%m-%e-%y %H:%M")
+  end
+
+  def full_name
+    first + " " + last
+  end
+
+  def family_name
+    last + " " + first
+  end
+
 end
